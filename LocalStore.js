9134ec8fe927e35ee7c8a4fc0fbcf1b13db2191a
Aurel Hermand <github@devone.de>
2014-04-26 11:06:15 +0200

    simplizierung und korrektur von get()

--- LocalStore.js
+++ LocalStore.js
@@ -32,10 +32,10 @@ var LocalStore = {
 		this.name = name;
 		this.prefix = options && 'prefix' in options ? options.prefix : '';
 		this.time = options && 'time' in options ? options.time : null;
-		var index = JSON.parse(localStorage.getItem(this.prefix+'Index-'+this.name)) || [];
-		localStorage.setItem(this.prefix+'Index-'+this.name, JSON.stringify(index));
+		var index = JSON.parse(localStorage.getItem(this.getIndexName())) || [];
+		localStorage.setItem(this.getIndexName(), JSON.stringify(index));
 		for (var i in index) {
-			var item = JSON.parse(localStorage.getItem(this.prefix+'Item-'+this.name+'-'+index[i]));
+			var item = JSON.parse(localStorage.getItem(this.getItemName(index[i])));
 			var time = (item && 'time' in item && 'setTime' in item) ? item.time : this.time;
 			if (time && item.setTime + time > this._dateNow()) {
 				this.remove(index[i]);
@@ -46,31 +46,40 @@ var LocalStore = {
 
 	getItem: function(key){this.get(key);},
 	get: function(key) {
-		return JSON.parse(localStorage.getItem(this.prefix+'Item-'+this.name+'-'+key)).data;
+		var item = JSON.parse(localStorage.getItem(this.getItemName(key)));
+		return item && 'data' in item ? item.data : null;
 	},
 
 	setItem: function(key, val, time){this.get(key, val, time);},
 	set: function(key, val, time) {
-		var index = JSON.parse(localStorage.getItem(this.prefix+'Index-'+this.name)) || [];
+		var index = JSON.parse(localStorage.getItem(this.getIndexName())) || [];
 		if (index.indexOf(key) === -1) { index.push(key); }
-		localStorage.setItem(this.prefix+'Index-'+this.name, JSON.stringify(index));
-		localStorage.setItem(this.prefix+'Item-'+this.name+'-'+key, JSON.stringify({data:val, setTime:this._dateNow(), time:time}));
+		localStorage.setItem(this.getIndexName(), JSON.stringify(index));
+		localStorage.setItem(this.getItemName(key), JSON.stringify({data:val, setTime:this._dateNow(), time:time}));
 	},
 
 	removeItem: function(key){this.remove(key);},
 	remove: function(key){
-		var index = JSON.parse(localStorage.getItem(this.prefix+'Index-'+this.name)) || []; // [key1, key2,...]
+		var index = JSON.parse(localStorage.getItem(this.getIndexName())) || []; // [key1, key2,...]
 		if (index.indexOf(key) !== -1) {
 			index = index.splice(key, 1);
-			localStorage.setItem(this.prefix+'Index-'+this.name, JSON.stringify(index));
+			localStorage.setItem(this.getIndexName(), JSON.stringify(index));
 		}
-		localStorage.removeItem(this.prefix+'Item-'+this.name+'-'+key);
+		localStorage.removeItem(this.getItemName(key));
 	},
 
 	clear: function() {
-		var index = JSON.parse(localStorage.getItem(this.prefix+'Index-'+this.name)) || [];
-		for (var i in index) {localStorage.removeItem(this.prefix+'Item-'+this.name+'-'+index[i]);}
-		localStorage.removeItem(this.prefix+'Index-'+this.name);
+		var index = JSON.parse(localStorage.getItem(this.getIndexName())) || [];
+		for (var i in index) {localStorage.removeItem(this.getItemName(index[i]));}
+		localStorage.removeItem(this.getIndexName());
+	},
+
+	getIndexName: function() {
+		return this.prefix + 'Index-' + this.name;
+	},
+
+	getItemName: function(key) {
+		return this.prefix + 'Item-' + this.name + '-' + key;
 	},
 
 	_dateNow: function() {
