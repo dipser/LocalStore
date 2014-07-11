34995eb4baaa82356ab9be69196550e299c4de29
Aurel Hermand <github@devone.de>
2014-07-11 11:43:12 +0200

    Listener Integration (kaputt)

--- LocalStore.js
+++ LocalStore.js
@@ -50,12 +50,26 @@ var LocalStore = {
 		return item && 'data' in item ? item.data : null;
 	},
 
-	setItem: function(key, val, time){this.get(key, val, time);},
-	set: function(key, val, time) {
+	getRawItem: function(key) {
+		var item = JSON.parse(localStorage.getItem(this.getItemName(key)));
+		//console.log(key, this.getItemName(key));
+		return item ? item : null;
+	},
+
+	setItem: function(key, val, time, listen){this.get(key, val, time, listen);},
+	set: function(key, val, time, listen) {
+		var oldRawItem = this.getRawItem(key);
 		var index = JSON.parse(localStorage.getItem(this.getIndexName())) || [];
 		if (index.indexOf(key) === -1) { index.push(key); }
 		localStorage.setItem(this.getIndexName(), JSON.stringify(index));
-		localStorage.setItem(this.getItemName(key), JSON.stringify({data:val, setTime:this._dateNow(), time:time}));
+		var listen = listen ? listen.toString() : null;
+		var data = {data:val, setTime:this._dateNow(), time:time, listen:listen};
+		localStorage.setItem(this.getItemName(key), JSON.stringify(data));
+		if (listen && !this.listened) { this._listen(); this.listened = true; }
+		//console.log('Verleich: ', oldRawItem.data, data.data);
+		if (oldRawItem && 'data' in oldRawItem && oldRawItem.data != data.data) { // vergleich is falsch... objekte vergleichen
+			//this._listenHandler({key:this.getItemName(key), oldValue:JSON.stringify(rawItem), newValue:JSON.stringify(data), url:location.hostname});
+		}
 	},
 
 	removeItem: function(key){this.remove(key);},
@@ -82,9 +96,33 @@ var LocalStore = {
 		return this.prefix + 'Item-' + this.name + '-' + key;
 	},
 
+	_listen: function() {
+		var self = this;
+		if (window.addEventListener) {
+			window.addEventListener("storage", function(e){self._listenHandler(e);}, false);
+		} else {
+			window.attachEvent("onstorage", function(e){self._listenHandler(e);});
+		};
+	},
+
+	_listenHandler: function(e) {
+		//console.log('listenerCalled', e);
+		if (!e) { e = window.event; }
+		var key = e.key.split(this.getIndexName()).join('').split(this.getItemName('')).join('');
+		var rawOldValue = JSON.parse(e.oldValue);
+		var rawNewValue = JSON.parse(e.newValue);
+		if (key.length>0 && rawOldValue!=null && rawNewValue!=null) {
+			if ('listen' in rawNewValue) {
+				console.log(key);
+				//console.log(key, rawOldValue, rawNewValue, typeof rawOldValue, typeof rawNewValue);
+				new Function('('+rawNewValue.listen+')()')();
+			}
+		}
+	},
+
 	_dateNow: function() {
 		return ((Date.now) ? Date.now() : (new Date()).getTime());
-	}
+	}	
 };
 
 
